054a40aca8419ffa4ca6ba62ac9c3a690a0931c6
savisalanjala <saviezhilarasan17@gmail.com>
2025-10-23 14:57:58 +0530

    Update 06.prime no.java

--- 06.prime no.java
+++ 06.prime no.java
@@ -1,1 +1,60 @@
 
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;  // numbers <= 1 are not prime
+        } else {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            System.out.println(number + " is a prime number.");
+        else
+            System.out.println(number + " is not a prime number.");
+    }
+}
+
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            System.out.println(number + " is a prime number.");
+        else
+            System.out.println(number + " is not a prime number.");
+    }
+}
