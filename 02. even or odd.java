86e6f445bfc23f7ee099a8c316a378a27e4ad7e0
savisalanjala <saviezhilarasan17@gmail.com>
2025-10-23 14:40:32 +0530

    Update 02. even or odd.java

--- 02. even or odd.java
+++ 02. even or odd.java
@@ -25,3 +25,33 @@ public class Main
 		
 	}
 }
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        
+        String result = (number % 2 == 0) ? "even" : "odd";
+        System.out.println("It is an " + result + " number");
+    }
+}
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if ((number & 1) == 0)
+            System.out.println("It is an even number");
+        else
+            System.out.println("It is an odd number");
+    }
+}
