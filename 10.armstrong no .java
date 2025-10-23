32fec9e9079abab54a8895d4336110999d63b67e
savisalanjala <saviezhilarasan17@gmail.com>
2025-10-23 15:18:03 +0530

    Create 10.armstrong no .java

---
+++ 10.armstrong no .java
@@ -0,0 +1,55 @@
+public class ArmstrongNumber {
+    public static boolean isArmstrong(int num) {
+        int original = num;
+        int sum = 0;
+        int digits = String.valueOf(num).length();  // number of digits
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num /= 10;
+        }
+
+        return sum == original;
+    }
+
+    public static void main(String[] args) {
+        int number = 153;
+
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is NOT an Armstrong number.");
+        }
+    }
+}
+
+
+
+public class ArmstrongRange {
+    public static boolean isArmstrong(int num) {
+        int original = num;
+        int sum = 0;
+        int digits = String.valueOf(num).length();
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num /= 10;
+        }
+
+        return sum == original;
+    }
+
+    public static void main(String[] args) {
+        int start = 100;
+        int end = 9999;
+
+        System.out.println("Armstrong numbers between " + start + " and " + end + ":");
+        for (int i = start; i <= end; i++) {
+            if (isArmstrong(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+}
