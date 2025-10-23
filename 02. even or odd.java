d8fcac6484ef4c39ff1ecb1ad1c52a88dc85b275
savisalanjala <saviezhilarasan17@gmail.com>
2025-10-23 14:37:56 +0530

    Create 02. even or odd.java

---
+++ 02. even or odd.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    System.out.print("Enter a number : ");
+	    
+	    int number = sc.nextInt();
+	    
+	    if(number%2==0){
+   
+	       System.out.print("It is a even number");
+	  
+	        
+	    }
+	    
+	    else{
+	            System.out.print("It is a odd number");
+	        }
+	    
+	    
+		
+	}
+}
